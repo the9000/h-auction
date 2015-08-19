a61880cbaf53ce3e13470d67523b8707bf5c6263
Dmitry Cheryasov <dcheryasov@google.com>
2015-08-19 04:36:53 -0400

    More BidderEngine tests done.

--- auction-java/engine/src/hackerati/task/impl/BidderEngineTest.java
+++ auction-java/engine/src/hackerati/task/impl/BidderEngineTest.java
@@ -3,6 +3,7 @@ package hackerati.task.impl;
 import hackerati.task.AdminEngine;
 import hackerati.task.AuctionPhase;
 import hackerati.task.BiddingEngine;
+import hackerati.task.InvalidBidError;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -27,14 +28,42 @@ public class BidderEngineTest {
     myAdminEngine = new AdminTerminal(myKVStore, ourLock);
   }
 
+  private void createActiveItem(String item_name) {
+    myAdminEngine.enlistItem(item_name, 50);
+    myAdminEngine.startAuction(item_name);
+  }
+
   @Test
   public void testPlaceNewBidWork() {
     String item_name = "item";
-    myAdminEngine.enlistItem(item_name, 50);
-    myAdminEngine.startAuction(item_name);
-    myEngine.placeBid(item_name, "bidder", 100);
+    createActiveItem(item_name);
+    myEngine.placeBid(item_name, "foo", 100);
     CompleteAuctionStatusImpl status = myKVStore.get(item_name);
     assertNotNull(status);
     assertEquals(new Integer(100), status.getLastBid());
+  }
+
+  @Test
+  public void testSequentialGrowingBidsWork() {
+    String item_name = "item";
+    createActiveItem(item_name);
+    myEngine.placeBid(item_name, "foo", 100);
+    myEngine.placeBid(item_name, "bar", 120);
+  }
+
+  @Test(expected= InvalidBidError.class)
+  public void testBidBelowLastBidFails() {
+    String item_name = "item";
+    createActiveItem(item_name);
+    myEngine.placeBid(item_name, "foo", 100);
+    myEngine.placeBid(item_name, "bar", 90);
+  }
+
+  @Test(expected= InvalidBidError.class)
+  public void testBidEqualToLastBidFails() {
+    String item_name = "item";
+    createActiveItem(item_name);
+    myEngine.placeBid(item_name, "foo", 100);
+    myEngine.placeBid(item_name, "bar", 100);
   }
 }
